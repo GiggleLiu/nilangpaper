0209167b7f9f78845dfb995b6d712a0f5dc3d306
GiggleLiu <cacate0129@gmail.com>
2020-03-09 13:36:51 +0800

    new reverse diff hessian

--- codes/bench_graphembedding.jl
+++ codes/bench_graphembedding.jl
@@ -102,6 +102,18 @@ function get_hessian(params0::AbstractArray{T}) where T
     hes
 end
 
+function get_hessian_rd(params0::AbstractArray{T}) where T
+    N = length(params0)
+    params = Dual.(params0, zero(T))
+    hes = zeros(T, N, N)
+    for i=1:N
+        @inbounds i !== 1 && (params[i-1] = Dual(params0[i-1], zero(T)))
+        @inbounds params[i] = Dual(params0[i], one(T))
+        res = ReverseDiff.gradient(loss, params)
+        hes[:,i] .= vec(ForwardDiff.partials.(res, 1))
+    end
+    hes
+end
 
 ################## irreversible ##################
 using Statistics, LinearAlgebra
